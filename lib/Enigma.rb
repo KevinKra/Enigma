68ba9722ff282c279557ba80a6ad2b706e502b6b
Kevin Krato <kratok08@gmail.com>
2019-11-01 12:45:36 -0400

    Write initial methods and associated comments

--- lib/Enigma.rb
+++ lib/Enigma.rb
@@ -1,3 +1,15 @@
-class Enigma
+require_relative "../lib/utils/helpers.rb"
 
+class Enigma
+  include Helpers
+  def encrypt(message, key = key_gen, date = Date::today)
+    # Each shift will be the sum of two other shifts known as the Keys and the Offsets
+    # A shift will be the A key plus the A offset
+    # B shift will be the B key plus the B offset, etc.
+  end
+
+  def decrypt(message, key, date = Date::today)
+    # decrypt the message
+    # ex: enigma.decrypt(encrypted[:encryption], "02715")
+  end
 end
