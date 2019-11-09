10360c763cb37f8b44db9daf334eb361926a40ce
Kevin Krato <kratok08@gmail.com>
2019-11-09 14:42:16 -0700

    Implement method to combine key and offset arrays and output integer

--- lib/Generator.rb
+++ lib/Generator.rb
@@ -5,7 +5,22 @@ class Generator
     end.map { |block| block.join.to_i }
   end
 
-  def self.gen_offset(date)
-    # generate offset here
+  def self.gen_date_offset(date)
+    date_squared = (date.to_i) ** 2
+    date_squared.to_s[-4..-1].chars.map { |elem| elem.to_i }
+  end
+
+  def self.gen_shift(key, date)
+    # generate a shift from the combined array output, convert to integer
+    combine_key_date(key, date).reduce([]) do |accum, array|
+     accum << array.sum
+    end.join.to_i
+  end
+
+  def self.combine_key_date(key, date)
+    # combine the generated key and generated offset arrays
+    key = gen_key(key)
+    offset = gen_date_offset(date)
+    key.zip(offset)
   end
 end
