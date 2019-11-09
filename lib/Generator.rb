8a7213fcef8c4525963cad713e0f90cc57da6139
Kevin Krato <kratok08@gmail.com>
2019-11-09 13:04:06 -0700

    Create Generator class and write key generating method

--- lib/Generator.rb
+++ lib/Generator.rb
@@ -1,6 +1,8 @@
 class Generator
   def self.gen_key(key)
-    # generate keys here
+    key.chars.each_cons(2).reduce([]) do |accum, block| 
+      accum << block
+    end.map { |block| block.join.to_i }
   end
 
   def self.gen_offset(date)
