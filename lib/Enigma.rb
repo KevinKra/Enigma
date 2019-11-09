71647da84a3887711bd531521deb7ad463ee74fc
Kevin Krato <kratok08@gmail.com>
2019-11-09 12:38:57 -0700

    Add class methods that generate keys and the current date

--- lib/Enigma.rb
+++ lib/Enigma.rb
@@ -1,15 +1,30 @@
+require 'time'
 require_relative "../lib/utils/helpers.rb"
 
 class Enigma
   include Helpers
-  def encrypt(message, key = key_gen, date = Date::today)
-    # Each shift will be the sum of two other shifts known as the Keys and the Offsets
-    # A shift will be the A key plus the A offset
-    # B shift will be the B key plus the B offset, etc.
+
+  def initialize
+    # @key = self.generate_key
+    # @date = self.generate_date
   end
 
-  def decrypt(message, key, date = Date::today)
-    # decrypt the message
-    # ex: enigma.decrypt(encrypted[:encryption], "02715")
+  def self.generate_key
+    rand.to_s[2..6]
   end
+
+  def self.generate_date
+    Time.now.strftime("%d%m%y")
+  end
+
+  # def encrypt(message, key = key_gen )
+  #   # Each shift will be the sum of two other shifts known as the Keys and the Offsets
+  #   # A shift will be the A key plus the A offset
+  #   # B shift will be the B key plus the B offset, etc.
+  # end
+
+  # def decrypt(message, key, date = Time.now.strftime("%d%m%y"))
+  #   # decrypt the message
+  #   # ex: enigma.decrypt(encrypted[:encryption], "02715")
+  # end
 end
