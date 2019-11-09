8c9ec254e4ebdaa893b112fc93ff86b095730d7c
Kevin Krato <kratok08@gmail.com>
2019-11-09 14:42:39 -0700

    Write tests for generator class methods

--- test/generator_test.rb
+++ test/generator_test.rb
@@ -7,6 +7,8 @@ require_relative "../lib/Generator.rb"
 class GeneratorTest < Minitest::Test
   def setup
     @generator = Generator.new
+    @key = "12345"
+    @date = "121219"
   end
 
   def test_it_exists
@@ -14,6 +16,18 @@ class GeneratorTest < Minitest::Test
   end
 
   def test_it_generates_keys
-    assert_equal [12, 23, 34, 45], Generator.gen_key("12345")
+    assert_equal [12, 23, 34, 45], Generator.gen_key(@key)
+  end
+
+  def test_it_generates_keys
+    assert_equal [5, 9, 6, 1], Generator.gen_date_offset(@date)
+  end
+
+  def test_it_combines_key_date
+    assert_equal [[12, 5], [23, 9], [34, 6], [45, 1]], Generator.combine_key_date(@key, @date)
+  end
+
+  def test_it_generates_a_shift
+    assert_equal 17324046, Generator.gen_shift(@key, @date)
   end
 end
