298d2d3a60f15a284cabda2b28c42a0b7a23060a
Kevin Krato <kratok08@gmail.com>
2019-11-12 07:23:13 -0700

    Remove methods from helper class

--- lib/utils/helpers.rb
+++ lib/utils/helpers.rb
@@ -1,13 +1,3 @@
 module Helpers
-  def key_gen
-    gen_array = []
-    while (gen_array.length != 5)
-      # generate a random number between 0,9
-      # push number into array
-      gen_array << 1
-    end
-    output = gen_array.join.to_s
-    # convert array of numbers to string
-    # return string
-  end
+
 end
