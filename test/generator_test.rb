31ab61b98359d9d6d0ac68713051f94f6130489a
Kevin Krato <kratok08@gmail.com>
2019-11-12 07:24:50 -0700

    Write tests for generator class

--- test/generator_test.rb
+++ test/generator_test.rb
@@ -16,18 +16,18 @@ class GeneratorTest < Minitest::Test
   end
 
   def test_it_generates_keys
-    assert_equal [12, 23, 34, 45], Generator.gen_key(@key)
+    assert_equal [12, 23, 34, 45], @generator.gen_key(@key)
   end
 
   def test_it_generates_offsets
-    assert_equal [5, 9, 6, 1], Generator.gen_date_offset(@date)
+    assert_equal [5, 9, 6, 1], @generator.gen_date_offset(@date)
   end
 
   def test_it_combines_key_date
-    assert_equal [[12, 5], [23, 9], [34, 6], [45, 1]], Generator.combine_key_date(@key, @date)
+    assert_equal [[12, 5], [23, 9], [34, 6], [45, 1]], @generator.combine_key_date(@key, @date)
   end
 
   def test_it_generates_a_shift
-    assert_equal 17324046, Generator.gen_shift(@key, @date)
+    assert_equal [17, 32, 40, 46], @generator.gen_shift(@key, @date)
   end
 end
